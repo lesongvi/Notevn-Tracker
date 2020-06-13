748754ebf866d5731bf7952b8b655ac1c05ebab0
Lê Song Vĩ <rappervn88@gmail.com>
2020-06-13 22:59:39 +0700

    New limit value updated

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.8d94d67bdfffda3c48c6dffb77da48e7.js"
+  "precache-manifest.5ac7dca0caaaa35a26fc7eff4b2b0285.js"
 );
 
 self.addEventListener('message', (event) => {
