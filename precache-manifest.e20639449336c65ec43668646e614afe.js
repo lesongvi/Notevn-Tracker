55020324ace47ba0c3d30f0d4a7fa366b5d4e014
lesongvi <vi@maxmines.com>
2022-12-07 16:47:59 +0700

    update v0.1.6

---
+++ precache-manifest.e20639449336c65ec43668646e614afe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0077cefd6c38073b13f3",
+    "url": "app.bundle.js"
+  },
+  {
+    "revision": "123bee293281e54b0fd1207f5549af56",
+    "url": "app.bundle.js.LICENSE"
+  },
+  {
+    "revision": "540c60cfa791179138c2",
+    "url": "background.bundle.js"
+  },
+  {
+    "revision": "9fadcda30b07120e2cb70b5a003acff9",
+    "url": "background.bundle.js.LICENSE"
+  },
+  {
+    "revision": "a7904cd9475138b95042",
+    "url": "contentScript.bundle.js"
+  },
+  {
+    "revision": "d355deb02b57271e2a1f7a3e24a9cc31",
+    "url": "popup.html"
+  },
+  {
+    "revision": "0077cefd6c38073b13f3",
+    "url": "static/css/app.f571184e.css"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "static/media/ionicons.621bd386.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.db178eae70e12fd4fd1b6552d7dbb5b9.js"
+  "precache-manifest.e20639449336c65ec43668646e614afe.js"
 );
 
 self.addEventListener('message', (event) => {
